057f0596ab028ded8b382aaa0facf60c40436350
Anonymouseyy <79242981+Anonymouseyy@users.noreply.github.com>
2022-01-19 20:58:38 -0800

    Added a background and blitted it to the screen. Also added functionality to start the game with a press of a key.

--- main.py
+++ main.py
@@ -4,13 +4,24 @@ from assets import MultiLineLabel, Label
 
 
 def home_screen():
+    title = Label("TETRIS", 50, white, (width/2, height/5))
+    cont = Label("(Press any key to play)", 15, white, (width / 2, height / 5 + 30))
+    bg = pygame.image.load("tetris_bg.png")
+
     while 1:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.KEYDOWN:
+                pass
+                game()
 
         screen.fill(black)
+        screen.blit(bg, (0, 0))
+        title.draw(screen)
+        cont.draw(screen)
         pygame.display.update()
 
 
@@ -29,8 +40,7 @@ def game():
 
 
 if __name__ == '__main__':
-
-    size = width, height = 300, 500
+    size = width, height = 500, 600
     white = (255, 255, 255)
     black = (0, 0, 0)
     green = (0, 255, 0)
@@ -39,6 +49,7 @@ if __name__ == '__main__':
     pygame.init()
 
     screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Tetris')
 
     home_screen()
 
